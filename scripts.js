92d4186f0bf3d44df5f030d4a983e3906961ce9d
Shawn Kebel <fxrender@outlook.com>
2020-12-20 01:34:00 -0600

    Add function to display items from 'form input'

--- scripts.js
+++ scripts.js
@@ -2,8 +2,19 @@ const formElem = document.getElementById('form');
 const todoInputElem = document.getElementById('todoInput');
 const todoListContainer = document.querySelector('.todo_list');
 
-
-
+// Add functions
+function displayTodoDOM(todo) {
+  const liElem = document.createElement("li");
+  liElem.innerHTML = `
+    <span class="text">${todo}</span>
+    <div class="options">
+        <span id="check"><i class="fa fa-check"></i></span>
+        <span id="edit"><i class="fa fa-edit"></i></span>
+        <span id="trash"><i class="fa fa-trash"></i></span>
+    </div>
+  `;
+  todoListContainer.appendChild(liElem);
+}
 
 
 
@@ -15,7 +26,7 @@ formElem.addEventListener('submit', (e) => {
   if(!inputTodo) {
     alert("Please enter a 'todo' item!");
   } else {
-    displayTodoDom(inputTodo);
+    displayTodoDOM(inputTodo);
   }
   console.log(inputTodo);
 });
